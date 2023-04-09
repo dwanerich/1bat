a313493324192252d5360269fe261adccacbcc63
Dwane Richards <drichards@pagerduty.com>
2023-04-09 00:30:27 -0400

    logo landing page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import logo from './1batlogo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="img" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        © 2023 1 BAT | 1 Brick at A Time
         </p>
         <a
           className="App-link"
@@ -15,7 +15,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
