dbdf75446abd819cc45276807387834ee1097eed
Dwane Richards <drichards@pagerduty.com>
2023-04-09 01:47:45 -0400

    border radius on logo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="img" alt="logo" />
+        <img src={logo} className="logo" alt="logo" />
         <p>
         © 2023 1 BAT | 1 Brick at A Time
         </p>
